9670c9ad44f0c1e4c4d2386d484a8fb30cb2eb42
Donat <donat.demanet@worldline.com>
2024-08-28 13:11:11 +0000

    add swagger

--- WLOpenBankingClient/test/callConsent.py
+++ WLOpenBankingClient/test/callConsent.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import datetime
 from pprint import pprint
@@ -16,14 +16,18 @@ configuration = swagger_client.Configuration()
 configuration.debug = True
 
 api_client = swagger_client.ApiClient(configuration)
-api_client.set_default_header('Authorization', 'Bearer d5bd895a4080dbbb469a207460b6fca')
+api_client.set_default_header("Authorization", "Bearer d5bd895a4080dbbb469a207460b6fca")
 
 api_instance = swagger_client.AccountInformationServiceApi(api_client)
-psuid = '123456'
-aspspid = '20101'
+psuid = "123456"
+aspspid = "20116"
 
 body = swagger_client.ConsentRequest()
-body.permissions = [PermissionEnum.ACCOUNTS, PermissionEnum.BALANCES, PermissionEnum.TRANSACTIONS]
+body.permissions = [
+    PermissionEnum.ACCOUNTS,
+    PermissionEnum.BALANCES,
+    PermissionEnum.TRANSACTIONS,
+]
 body.psu_data = swagger_client.PsuData()
 body.psu_data.aspsp_id = aspspid
 pprint(body)
@@ -31,7 +35,9 @@ pprint(body)
 try:
     # Requests by user id
     xreqid = str(uuid.uuid4())
-    api_response = api_instance.consent_extended(xreqid, datetime.datetime.now(), psuid, body = body)
+    api_response = api_instance.consent_extended(
+        xreqid, datetime.datetime.now(), psuid, body=body
+    )
 
     print("## answer ##")
     pprint(api_response)
@@ -40,4 +46,6 @@ try:
     print(api_response.links.redirect_url.href)
     print(api_response.consent_id)
 except ApiException as e:
-    print("Exception when calling AccountInformationServiceApi.consent_extended: %s\n" % e)
+    print(
+        "Exception when calling AccountInformationServiceApi.consent_extended: %s\n" % e
+    )
--- WLOpenBankingClient/test/callListAccounts.py
+++ WLOpenBankingClient/test/callListAccounts.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import datetime
 from pprint import pprint
@@ -15,17 +15,19 @@ configuration = swagger_client.Configuration()
 configuration.debug = True
 
 api_client = swagger_client.ApiClient(configuration)
-api_client.set_default_header('Authorization', 'Bearer d5bd895a4080dbbb469a207460b6fca')
+api_client.set_default_header("Authorization", "Bearer d5bd895a4080dbbb469a207460b6fca")
 
 api_instance = swagger_client.AccountInformationServiceApi(api_client)
 
 try:
     # Requests by user id
     xreqid = str(uuid.uuid4())
-    psuid = '123456'
-    consentid = '1126450'
-    aspspid = '20101'
-    api_response = api_instance.accounts(psuid, aspspid, xreqid, datetime.datetime.now())
+    psuid = "123456"
+    consentid = "1126569"
+    aspspid = "20116"
+    api_response = api_instance.accounts(
+        psuid, aspspid, xreqid, datetime.datetime.now()
+    )
 
     print("## answer ##")
     pprint(api_response)
@@ -33,4 +35,6 @@ try:
 
     print(api_response.accounts[0].account_id)
 except ApiException as e:
-    print("Exception when calling AccountInformationServiceApi.consent_extended: %s\n" % e)
+    print(
+        "Exception when calling AccountInformationServiceApi.consent_extended: %s\n" % e
+    )
